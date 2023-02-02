47644d377d5cf2f6f91337cf8bcf4ed688a1fe99
rjdep <89678449+rjdep@users.noreply.github.com>
2023-02-02 21:13:27 +0530

    Create Tweet Counts Per Frequency.cpp

---
+++ Amazon/28_01_2023/Tweet Counts Per Frequency.cpp
@@ -0,0 +1,30 @@
+class TweetCounts {
+public:
+    TweetCounts() {
+    }
+    
+    void recordTweet(string tweetName, int time) {
+        tweets_[tweetName][time]++;
+    }
+    
+    vector<int> getTweetCountsPerFrequency(string freq, string tweetName, int startTime, int endTime) {
+        int interval = freq[0] == 'm' ? 60 : (freq[0] == 'h' ? 3600 : 86400);
+        const auto& times = tweets_[tweetName];
+        vector<int> result(((endTime - startTime) / interval) + 1);
+        const auto& end = times.upper_bound(endTime);
+        for (auto it = times.lower_bound(startTime); it != end; it++) {
+           result[(it->first - startTime) / interval] += it->second;
+        }
+        return result;
+    }
+    
+private:
+    unordered_map<string, map<int, int>> tweets_;
+};
+
+/**
+ * Your TweetCounts object will be instantiated and called as such:
+ * TweetCounts* obj = new TweetCounts();
+ * obj->recordTweet(tweetName,time);
+ * vector<int> param_2 = obj->getTweetCountsPerFrequency(freq,tweetName,startTime,endTime);
+ */
